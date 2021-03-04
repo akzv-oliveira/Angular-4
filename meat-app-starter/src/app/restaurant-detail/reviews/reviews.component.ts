c440f4a661a081865f1c8b1fa2dae9d16c96c70d
akzv-oliveira <vinicius.soliveira@bandtec.com.br>
2021-03-04 01:30:39 -0300

    adiciona a aplicação meat app feita em angular 4

---
+++ meat-app-starter/src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { RestaurantsService } from '../../restaurants/restaurants.service'
+import { ActivatedRoute } from '@angular/router'
+
+@Component({
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewsComponent implements OnInit {
+
+  reviews: Observable<any>
+
+  constructor(private RestaurantsService: RestaurantsService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.reviews = this.RestaurantsService.reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
+  }
+
+}
